bfaf28dea527888eeb7a9a72df8f4e3665c8d9c5
ihsan <aliihsanturk@gmail.com>
2019-05-22 21:04:05 +0300

    deleted unnecessary rules, decreased window resize

--- config.h
+++ config.h
@@ -21,18 +21,12 @@ static const char *colors[][3]     = {
   [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
+static const Rule rules[] = {
+
+};
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-
-static const Rule rules[] = {
-  /* xprop(1):
-   *  WM_CLASS(STRING) = instance, class
-   *  WM_NAME(STRING) = title
-   */
-  /* class      instance    title       tags mask     isfloating   monitor */
-  { "Gimp",     NULL,       NULL,       0,            1,           -1 },
-  { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-};
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
@@ -83,8 +77,8 @@ static Key keys[] = {
   { MODKEY,           XK_k,      focusstack,     {.i = -1 } },
   { MODKEY,           XK_i,      incnmaster,     {.i = +1 } },
   { MODKEY,           XK_d,      incnmaster,     {.i = -1 } },
-  { MODKEY,           XK_h,      setmfact,       {.f = -0.05} },
-  { MODKEY,           XK_l,      setmfact,       {.f = +0.05} },
+  { MODKEY,           XK_h,      setmfact,       {.f = -0.03} },
+  { MODKEY,           XK_l,      setmfact,       {.f = +0.03} },
   { MODKEY,           XK_Return, zoom,           {0} },
   { MODKEY,           XK_Tab,    view,           {0} },
   { MODKEY|ShiftMask, XK_c,      killclient,     {0} },
